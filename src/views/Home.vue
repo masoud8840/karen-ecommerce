ca3d7c83713cd32ebcc5f3787bde64f5f50f7c80
Masoud Gharedaghi <masoudgharedaghi@yahoo.com>
2024-01-28 16:44:33 +0330

    add feature: hero showcase grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
   <main class="home-view">
-    <h1>Hello from Home.vue</h1>
+    <section class="categories-showcase container">
+      <article class="showcase1">
+        <h3>Showcase 1</h3>
+      </article>
+      <article class="showcase2">
+        <section class="showcase-overlay">
+          <router-link :to="{ name: 'Home' }">
+            جعبه های ابزار آماده کارن
+          </router-link>
+        </section>
+      </article>
+      <article class="showcase3">
+        <h3>Showcase 3</h3>
+      </article>
+      <article class="showcase4">
+        <h3>Showcase 4</h3>
+      </article>
+    </section>
   </main>
 </template>
