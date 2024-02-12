3098bbd7e3ff71ff0df65929625f4e4c501a97b5
Masoud Gharedaghi <masoudgharedaghi@yahoo.com>
2024-02-12 14:57:31 +0330

    add section: user-account dashboard

---
+++ src/components/icons/Plus.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg
+    :width="props.width"
+    :height="props.height || props.width"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.2159 5.375V13.2159H5.375V14.7841H13.2159V22.625H14.7841V14.7841H22.625V13.2159H14.7841V5.375H13.2159Z"
+      fill="#39354C"
+    />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  width: {
+    type: Number,
+    required: true,
+  },
+  height: {
+    type: Number,
+    default: null,
+  },
+  color: {
+    type: String,
+    default: "#39354C",
+  },
+  hoverColor: {
+    type: String,
+    default: "#39354c",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+svg {
+  path {
+    fill: v-bind("props.color");
+  }
+  &:hover {
+    path {
+      fill: v-bind("props.hoverColor");
+    }
+  }
+}
+</style>
--- src/views/Dashboard/UserAccount.vue
+++ src/views/Dashboard/UserAccount.vue
@@ -1,3 +1,45 @@
 <template>
   <h1>this is useraccount.vue</h1>
+  <form class="user_account-view container">
+    <section class="input_group col full_name-input_group">
+      <label for="fullname" class="section-title orange"
+        >نام و نام خانوادگی</label
+      >
+      <input type="text" name="fullname" id="fullname" />
+    </section>
+    <section class="input_group col email_or_phone">
+      <label for="emailOrPhone" class="section-title orange"
+        >پست الکترونیکی یا شماره همراه</label
+      >
+      <input type="text" name="emailOrPhone" id="emailOrPhone" />
+    </section>
+    <section class="addresses-container">
+      <section class="addresses-header">
+        <h4 class="section-title orange">آدرس ها</h4>
+        <button type="button">
+          <Plus :width="24" hover-color="#fdac16" />
+        </button>
+      </section>
+      <section class="addresses-body">
+        <section class="addresses_item-container">
+          <section class="addresses_item-header">
+            <h5 class="addresses_item-title">محل کار</h5>
+            <section class="addresses_item-actions">
+              <button type="button">تصحیح</button>
+              <button type="submit" class="btn-primary">حذف</button>
+            </section>
+          </section>
+          <section class="address_item-body">
+            <p class="addresses_item-subtitle text-muted">
+              شهریار - فاز دو مخابرات - کوچه کوشش - کوچه کارا - پلاک 21
+            </p>
+          </section>
+        </section>
+      </section>
+    </section>
+  </form>
 </template>
+
+<script setup>
+import Plus from "../../components/icons/Plus.vue";
+</script>
