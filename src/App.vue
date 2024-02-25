142abd5fdc56621448392596263f6339b5892a67
Masoud Gharedaghi <masoudgharedaghi@yahoo.com>
2024-02-25 20:41:07 +0330

    responsive: dashboard navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
+import { provide, ref } from "vue";
 import TheFooter from "./components/layout/TheFooter.vue";
 import TheNavbar from "./components/layout/TheNavbar.vue";
+
+const isUserInMobileDevice = ref(true);
+window.addEventListener("resize", handleWindowResize);
+function handleWindowResize() {
+  isUserInMobileDevice.value = window.innerWidth < 768;
+}
+handleWindowResize();
+provide("responsive", isUserInMobileDevice);
 </script>
 
 <template>
--- src/components/layout/DashboardNavbar.vue
+++ src/components/layout/DashboardNavbar.vue
@@ -1,21 +1,31 @@
 <template>
   <header class="container">
     <nav>
-      <ul>
-        <li v-for="(menuItem, index) in menuItems" :key="index">
-          <router-link
-            :to="menuItem.link"
-            :class="{ active: $route.name === menuItem.link.name }"
-            >{{ menuItem.title }}</router-link
-          >
-        </li>
-      </ul>
+      <button class="navbar_toggler" @click="toggleNavbarToggler">
+        <Arrow :width="21" direction="down" />{{ currentActiveSubroute.title }}
+      </button>
+      <transition name="navbar" mode="in-out">
+        <ul v-if="isNavbarTogglerOpen">
+          <li v-for="(menuItem, index) in menuItems" :key="index">
+            <router-link
+              :to="menuItem.link"
+              :class="{ active: $route.name === menuItem.link.name }"
+              >{{ menuItem.title }}</router-link
+            >
+          </li>
+        </ul>
+      </transition>
     </nav>
   </header>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed, inject, watch } from "vue";
+import { useRoute, onBeforeRouteUpdate } from "vue-router";
+import Arrow from "../icons/Arrow.vue";
+
+const route = useRoute();
+
 const menuItems = ref([
   {
     title: "حساب کاربری",
@@ -34,4 +44,26 @@ const menuItems = ref([
     link: { name: "OrdersHistory" },
   },
 ]);
+
+const currentActiveSubroute = computed(() => {
+  return menuItems.value.find((menu) => menu.link.name === route.name);
+});
+
+onBeforeRouteUpdate((_, _1, next) => {
+  if (isMobile.value) {
+    isNavbarTogglerOpen.value = false;
+  }
+  next();
+});
+
+function toggleNavbarToggler() {
+  isNavbarTogglerOpen.value = !isNavbarTogglerOpen.value;
+}
+
+const isMobile = inject("responsive");
+watch(isMobile, (newValue) => {
+  isNavbarTogglerOpen.value = !newValue;
+});
+
+const isNavbarTogglerOpen = ref(!isMobile.value);
 </script>
